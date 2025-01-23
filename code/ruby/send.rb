bbce1a75072e3c3e9ca3f38275bc10adaef19d57
Iqbal Singh <99212226+isingh00@users.noreply.github.com>
2025-01-23 15:01:48 -0500

    fix ruby qoutes

--- code/ruby/send.rb
+++ code/ruby/send.rb
@@ -5,12 +5,12 @@ require 'base64'
 Dotenv.load
 def send_simple_message
   RestClient.post(
-    "${API_BASE_URL}/${YOUR_DOMAIN}/messages",
+    '${API_BASE_URL}/${YOUR_DOMAIN}/messages',
     {
-    from: "Mailgun Sandbox <postmaster@${YOUR_DOMAIN}>",
-	  to:  "${USER_NAME} <${USER_EMAIL}>",
-  	subject: "Hello ${USER_NAME}",
-  	text: "Congratulations ${USER_NAME}, you just sent an email with Mailgun! You are truly awesome!"
+    from: 'Mailgun Sandbox <postmaster@${YOUR_DOMAIN}>',
+	  to:  '${USER_NAME} <${USER_EMAIL}>',
+  	subject: 'Hello ${USER_NAME}',
+  	text: 'Congratulations ${USER_NAME}, you just sent an email with Mailgun! You are truly awesome!'
     },
     { Authorization: "Basic #{Base64.strict_encode64("api:#{ENV['API_KEY'] || 'API_KEY'}")}" }
   )
--- code/ruby/templates.rb
+++ code/ruby/templates.rb
@@ -5,13 +5,13 @@ require 'base64'
 Dotenv.load
 def send_simple_message_templates
 	RestClient.post(
-    "${API_BASE_URL}/${YOUR_DOMAIN}/messages",
+    '${API_BASE_URL}/${YOUR_DOMAIN}/messages',
     {
-    from: "Mailgun Sandbox <postmaster@${YOUR_DOMAIN}>",
-	  to: "${USER_NAME} <${USER_EMAIL}>",
-		subject: "Hello ${USER_NAME}",
-		template: "${TEMPLATE_NAME}",
-		":h:X-Mailgun-Variables": '{"test": "test"}'
+    from: 'Mailgun Sandbox <postmaster@${YOUR_DOMAIN}>',
+	  to: '${USER_NAME} <${USER_EMAIL}>',
+		subject: 'Hello ${USER_NAME}',
+		template: '${TEMPLATE_NAME}',
+		':h:X-Mailgun-Variables': '{"test": "test"}'
     },
     { Authorization: "Basic #{Base64.strict_encode64("api:#{ENV['API_KEY'] || 'API_KEY'}")}" }
   )
